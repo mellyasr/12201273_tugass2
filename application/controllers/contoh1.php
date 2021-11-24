c180dde5097e1ad8c78911797aed4f3f24db2a08
mellyasr <mellymeliasari125@gmail.com>
2021-11-24 12:15:49 +0700

    menambahkan tugas_2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+		<tittle>Biodata/tittle>
+</HEAD>
+<BODY>
+		<h2>Biodata  : <h2>
+		<p> Nama Lengkap		: Melly Meliasari </p>
+		<p> Alamat				: Kp.Cirarab RT 01/ RW 01 Kel. Cigantang Kec. Mangkubumi Tasikmalaya </p>
+		<p> No. HP				: 081992239549 </p>
+</BODY>
+</HTML>
